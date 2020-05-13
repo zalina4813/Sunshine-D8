5ee44848a61cbf9f42a3d52b80b1fbdcf5ee0a5b
Austin Kennedy <kennedyaustin06561@gmail.com>
2020-05-13 19:01:51 -0400

    put code onto HTML for modal

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,6 +9,14 @@ $('document').ready(function() {
         }
     
     })
+
+    var modal = document.getElementById('id01'); 
+  
+        window.onclick = function(event) { 
+            if (event.target == modal) { 
+                modal.style.display = "none"; 
+            } 
+        } 
     
 })
     

7a3c0c6322bc0bfc5fb4d639ea975f66cecc2ea0
Austin Kennedy <kennedyaustin06561@gmail.com>
2020-05-13 20:28:13 -0400

    deleted button tags on sign up confirmation to redirect user to website to use APIs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,6 +13,7 @@ $('document').ready(function() {
     var modal = document.getElementById('id01'); 
   
         window.onclick = function(event) { 
+            preventDefault();
             if (event.target == modal) { 
                 modal.style.display = "none"; 
             } 
